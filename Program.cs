fe0134f59896995efc4ef6142d86ef0a11469a1d
vivianemenezes571 <vivianemenezes5711@gmail.com>
2025-09-26 21:45:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string nome, sobrenome;
+Console.Write("Digite seu nome : ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome : ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome de catálogo: {sobrenome.ToUpper()}, {nome}");
+{
+    
+}
